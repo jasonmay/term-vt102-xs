ad6541e1a5ce97ac4bb1855ebb045a3d381e694c
Jason May <jason.a.may@gmail.com>
2011-03-14 20:55:58 -0400

    more adjusting

--- lib/Term/VT102/XS.pm
+++ lib/Term/VT102/XS.pm
@@ -1,31 +1,6 @@
 package Term::VT102::XS;
-
-use 5.012002;
 use strict;
 use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Term::VT102::XS ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
 
 require XSLoader;
 XSLoader::load('Term::VT102::XS', $VERSION);
