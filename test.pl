211186da44d11d81dff2494245fcc83baa668a64
Jason May <jason.a.may@gmail.com>
2011-03-19 19:50:29 -0400

    add my test.pl script because why not

---
+++ test.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use blib;
+use Term::VT102::XS;
+
+my $v = Term::VT102::XS->new();
+
+$v->process("hello\njasonmay");
+warn $v->row_plaintext(1);
+warn $v->row_plaintext(2);
+
